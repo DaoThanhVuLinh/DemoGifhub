86d6da44bc5edb34a8b6d201d62dc106cb78fc42
DaoThanhVuLinh <0306191241@caothang.edu.vn>
2022-10-06 15:09:01 +0700

    Add project files.

---
+++ DemoGifhud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoGifhud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
